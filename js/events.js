a81cdbf2fae3055b4864c46e3b08cfebb2af6fd1
Elisabeth Yang <betsyang123@gmail.com>
2018-07-02 03:42:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,12 @@
-//define functions here
-
+function getIt() {
+  $('p').on("click", function () {
+    alert("Hey!");}
+)}
+function frameit() {
+  $('img').on('load',function () {
+    $('img').addClass('tasty');
+  })
+}
 $(document).ready(function(){
 
 // call functions here
